aac38b76af4c9abf0aa9c1b23a28e09adb8d32d6
Juanitaag1 <35508373+Juanitaag1@users.noreply.github.com>
2018-10-30 17:38:44 -0700

    changedbuttoncolor

---
+++ app/src/main/java/com/example/poppi/campnow/ResultsMapActivity.java
@@ -0,0 +1,13 @@
+package com.example.poppi.campnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultsMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results_map);
+    }
+}
